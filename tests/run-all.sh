c0c326aecb8b404957fb3e5c10547a149cd97cce
Tobias Bollinger <tobias.bollinger@bs.ch>
2026-04-02 14:27:53 +0200

    fixed pr findings

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -7,6 +7,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Running all template tests..."
 echo ""
 
+"$SCRIPT_DIR/test-template-quick.sh"
+echo ""
 "$SCRIPT_DIR/test-template.sh"
 echo ""
 "$SCRIPT_DIR/test-template-azure.sh"
